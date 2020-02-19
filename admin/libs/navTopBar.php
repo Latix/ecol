1a20ed200504d3852a658b546b334bc3dfd8c915
Kamsi Kodi <Kamsikodi@gmail.com>
2020-02-19 17:35:43 +0100

    Fixed Frontend Look

--- admin/libs/navTopBar.php
+++ admin/libs/navTopBar.php
@@ -12,48 +12,18 @@
           <span class="icon-menu"></span>
         </button>
         <ul class="navbar-nav navbar-nav-left header-links d-none d-md-flex">
-
-          <?php if (isset($_SESSION['seller_id'])): ?>
-
-
-          <li class="nav-item active">
-            <a href="../index" class="nav-link"><i class="mdi mdi-calendar"></i>Shop</a>
-          </li>
-          <?php endif; ?>
         </ul>
         <ul class="navbar-nav navbar-nav-right">
-
-
           <li class="nav-item d-none d-lg-block color-setting">
             <a class="nav-link" href="#">
               <i class="mdi mdi-format-color-fill"></i>
             </a>
           </li>
-          <?php
-            $profilePic = "";
-            if(isset($_SESSION['student_id'])){
-              $profilePic = "s.jpg";
-            }
-            if(isset($_SESSION['retailer_id'])){
-              $profilePic = "r.jpg";
-            }
-            if(isset($_SESSION['lecturer_id'])){
-              $profilePic = "l.jpg";
-            }
-            if(isset($_SESSION['wholesaler_id']) || isset($_SESSION['admin_id'])){
-              $profilePic = "w.jpg";
-            }
-           ?>
           <li class="nav-item nav-settings d-none d-lg-block">
             <a class="nav-link" href="#">
-              <img class="img-xs rounded-circle" src="images/favicon.png" alt="">
+              <img class="img-xs rounded-circle" style="width: 26px;height: 26px;" src="images/favicon.png" alt="">
             </a>
           </li>
-          <?php if (isset($_SESSION['student_id']) || isset($_SESSION['lecturer_id'])): ?>
-          <li class="nav-item active">
-            <a href="../index" class="nav-link"><i class="mdi mdi-shop"></i>Shop</a>
-          </li>
-        <?php endif; ?>
           <li class="nav-item active">
             <a href="?action=logout" class="nav-link"><i class="fa fa-sign-out"></i>Logout</a>
           </li>
